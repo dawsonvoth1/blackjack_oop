9d4570d73a194a0ac4fa861cfd0bb23818261253
kewa4298 <kewa4298@colorado.edu>
2022-04-26 14:16:21 -0600

    Adding remove_player method

--- blackjack/game.cpp
+++ blackjack/game.cpp
@@ -1,5 +1,7 @@
 #include "game.h"
 #include "ui_game.h"//.h"
+#include "player.h"
+#include "vector"
 
 Game::Game(QWidget *parent)
     : QMainWindow(parent)
@@ -13,3 +15,13 @@ Game::~Game()
     delete ui;
 }
 
+/**
+ * @brief Game::remove_player remove a player from the players_ vector
+ * @param p the player to remove
+ */
+void Game::remove_player(Player* p) {
+    std::vector<Player*>::iterator position = std::find(players_.begin(), players_.end(), p);
+    if (position != players_.end()) {
+        players_.erase(position);
+    }
+}
