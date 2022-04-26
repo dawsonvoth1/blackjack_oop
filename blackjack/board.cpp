07ad903edd85e8e69471c2b44b5afe64db19b225
kewa4298 <kewa4298@colorado.edu>
2022-04-26 12:12:21 -0600

    Adding new Project

---
+++ blackjack/board.cpp
@@ -0,0 +1,15 @@
+#include "board.h"
+#include "ui_board.h"
+
+Board::Board(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::Board)
+{
+    ui->setupUi(this);
+}
+
+Board::~Board()
+{
+    delete ui;
+}
+
