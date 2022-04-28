af061469ef05f198bdc4b09c70e8cf58d1f3bfa3
dawsonvoth1 <70551506+dawsonvoth1@users.noreply.github.com>
2022-04-27 19:03:53 -0600

    png changes

--- blackjack/player.cpp
+++ blackjack/player.cpp
@@ -4,18 +4,18 @@ Player::Player(QGraphicsScene *scene, QObject *parent)
     : QObject{parent}
 {
     scene_=scene;
-//    QColor c1(255,255,255);
-//    QColor c2(0,0,0);
+    QColor c1(255,255,255);
+    QColor c2(0,0,0);
 
-//    QPen p(c1);
-//    QBrush b(c2, Qt::LinearGradientPattern);
+    QPen p(c1);
+    QBrush b(c2, Qt::LinearGradientPattern);
 
-//    x_=0;
-//    y_=350;
-//    width_=200;
-//    height_=50;
+    x_=0;
+    y_=350;
+    width_=200;
+    height_=50;
 
-//    QGraphicsRectItem *rec = scene_->addRect(x_, y_, width_, height_,p, b);
+    QGraphicsRectItem *rec = scene_->addRect(x_, y_, width_, height_,p, b);
 
 
 }
